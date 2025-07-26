aaf5b98f00a2f8d41d10fb7b9eaf10cf30c4d07a
The-Code-Doc <vihaangupta062014@gmail.com>
2025-07-26 10:50:43 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -26,11 +26,49 @@ window.onload = function () {
     const bgMusic = this.sound.add("bgMusic", { loop: true, volume: musicVol });
     bgMusic.play();
 
-    const clickBtn = this.add.text(300, 250, "Click Me", { fontSize: "32px", fill: "#000" }).setInteractive();
-    clickBtn.on("pointerdown", () => {
+    this.add.text(400, 60, "🌍 Word Islands", {
+      fontSize: "36px",
+      fill: "#003",
+      fontFamily: "Quicksand"
+    }).setOrigin(0.5);
+
+    const startBtn = this.add.text(400, 200, "Start Game", {
+      fontSize: "28px",
+      backgroundColor: "#00c2ff",
+      color: "#fff",
+      padding: { x: 20, y: 10 },
+      borderRadius: 10
+    }).setOrigin(0.5).setInteractive();
+
+    startBtn.on("pointerdown", () => {
       this.sound.play("click", { volume: sfxVol });
-      this.sound.play("correct", { volume: sfxVol });
-      this.sound.play("wrong", { volume: sfxVol });
+      showIslandScreen.call(this);
+    });
+  }
+
+  function showIslandScreen() {
+    this.add.rectangle(400, 300, 720, 480, 0xe0f7fa).setStrokeStyle(4, 0x004d40);
+    this.add.text(400, 100, "Choose an Island", {
+      fontSize: "32px",
+      fill: "#004d40",
+      fontFamily: "Quicksand"
+    }).setOrigin(0.5);
+
+    const islands = ["Earth", "Water", "Wind", "Fire", "Elemental"];
+    islands.forEach((island, i) => {
+      const btn = this.add.text(200 + i * 120, 250, island, {
+        fontSize: "20px",
+        backgroundColor: "#004d40",
+        color: "#fff",
+        padding: { x: 10, y: 5 }
+      }).setOrigin(0.5).setInteractive();
+
+      btn.on("pointerdown", () => {
+        this.sound.play("click", { volume: 0.7 });
+        this.add.text(400, 450, `You selected ${island} Island`, {
+          fontSize: "18px", fill: "#333"
+        }).setOrigin(0.5);
+      });
     });
   }
 };
